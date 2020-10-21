9344398bf031689b0b7cd9a3b03f1d2bbd05251c
Klestov1973 <72647242+Klestov1973@users.noreply.github.com>
2020-10-21 10:50:26 +0500

    no commit

--- proj-20201007-sdl2-1/main.cc
+++ proj-20201007-sdl2-1/main.cc
@@ -9,7 +9,7 @@
 #include <iostream>
 #include <memory>
 #include <cmath>
-#include <algorithm>
+
 
 #include <SDL2/SDL.h>
 
@@ -27,7 +27,7 @@ int main(int, char**)
 
 	auto win = std::shared_ptr<SDL_Window>(
 			SDL_CreateWindow(
-						"Окно нашего графического приложения",
+						"Cicle",
 						SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 						WIDTH, HEIGHT,
 						SDL_WINDOW_SHOWN),
@@ -59,14 +59,53 @@ int main(int, char**)
 		SDL_SetRenderDrawColor(ren.get(), 0, 48, 64, 255);
 		SDL_RenderClear(ren.get());
 
+
 		SDL_SetRenderDrawColor(ren.get(), 255, 255, 255, 255);
-		SDL_RenderDrawLine(ren.get(), 10, 10, 790, 590);
+
+		int centreX=300;
+		int centreY=400;
+		int radius = 100;
+		const int diameter = (radius * 2);
+
+		int x = (radius - 1);
+		int y = 0;
+		int tx = 1;
+		int ty = 1;
+		int error = (tx - diameter);
+
+		while (x >= y)
+		{
+		SDL_RenderDrawPoint(ren.get(), centreX + x, centreY - y);
+		SDL_RenderDrawPoint(ren.get(), centreX + x, centreY + y);
+		SDL_RenderDrawPoint(ren.get(), centreX - x, centreY - y);
+		SDL_RenderDrawPoint(ren.get(), centreX - x, centreY + y);
+		SDL_RenderDrawPoint(ren.get(), centreX + y, centreY - x);
+		SDL_RenderDrawPoint(ren.get(), centreX + y, centreY + x);
+		SDL_RenderDrawPoint(ren.get(), centreX - y, centreY - x);
+		SDL_RenderDrawPoint(ren.get(), centreX - y, centreY + x);
+
+		if (error <= 0)
+		{
+		++y;
+		error += ty;
+		ty += 2;
+		}
+
+		if (error > 0)
+		{
+		--x;
+		tx += 2;
+		error += (tx - diameter);
+		}
+		}
 
 
+
+			}
 		SDL_RenderPresent(ren.get());
-	}
+
 
 
 	return 0;
-}
+	}
 
